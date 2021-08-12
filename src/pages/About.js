ed0e1c679e4cf38b69ed8398881194087edff10a
Rashmi R <rashmiramesh18695@gmail.com>
2021-08-12 17:47:41 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sed nobis voluptatibus ex iusto? Nobis ut natus ullam commodi, consequuntur consequatur, voluptatibus alias cum porro accusantium eaque ducimus! Aut repellat voluptate repudiandae ex, eius fugiat aperiam non tenetur qui deserunt?</p>
+        </section>
+    );
+};
+
+export default About;
